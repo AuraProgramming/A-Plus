1d9471ed20c649191943c046594d2010d5bef6ae
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-06-19 14:28:01 -0400

    Update A-Plus.bat

--- A-Plus.bat
+++ A-Plus.bat
@@ -9,11 +9,15 @@ for /f "tokens=1" %%a in (%~f1) do (
   call :case-%%a 2>nul || call :command_default %%a
 )
 
+:case-PrintOn
+  @echo on
+  
+:case-PrintOff
+  @echo off
+  
 :case-cow
   echo moo
-  goto :eof
 
 	
 :command_default
-  echo Non recognisable command
   exit
