f2a7ffa15e6f287c69bf8da5952e7b170fe3cf74
AuraProgramming <83914971+AuraProgramming@users.noreply.github.com>
2021-06-19 22:49:34 -0400

    Update A-Plus.bat

--- A-Plus.bat
+++ A-Plus.bat
@@ -1,20 +1,19 @@
 if not exist "%~f1" (
-  exit
+	exit
 )
 
 for /f "tokens=1" %%a in (%~f1) do (
-  call :case-%%a 2>nul || call :command_default %%a
+	call :case-%%a 2>nul || call :command_default %%a
 )
 
 :case-PrintOn
-  @echo on
+	@echo on
   
 :case-PrintOff
-  @echo off
+	@echo off
   
 :case-cow
-  echo moo
-
+	echo moo
 	
 :command_default
-  exit
+	exit
